bbb0991159cc16442c12f8d9eff86c5b9d05b1a6
rdudhagra <rdudhagra@gmail.com>
2025-03-09 18:20:58 -0700

    Modify flash light strip controller script to be agnostic to usernames

--- Website/flash-light-strip-controller.sh
+++ Website/flash-light-strip-controller.sh
@@ -3,30 +3,30 @@
 ############# Install Arduino ###############
 
 # Install Arduino CLI
-if [ ! -f /home/pi/arduino-cli ]
+if [ ! -f "$HOME"/arduino-cli ]
 then
 	echo "arduino-cli not found! Installing..."
-    curl -fsSL https://raw.githubusercontent.com/arduino/arduino-cli/master/install.sh | BINDIR=/home/pi/ sh
-    chmod +x /home/pi/arduino-cli
+    curl -fsSL https://raw.githubusercontent.com/arduino/arduino-cli/master/install.sh | BINDIR=$HOME/ sh
+    chmod +x "$HOME"/arduino-cli
 fi
 
 # Install AVR Boards
-/home/pi/arduino-cli core install arduino:avr
+"$HOME"/arduino-cli core install arduino:avr
 
 # Install FastLED
-/home/pi/arduino-cli lib install FastLED
+"$HOME"/arduino-cli lib install FastLED
 
 ##############  Flash sketch  ###############
 
 # cd to the script's directory
-cd "${0%/*}"
+cd "${0%/*}" || exit
 
 # Compile
-/home/pi/arduino-cli compile -b arduino:avr:uno "../Light Strip Controller/main"
+"$HOME"/arduino-cli compile -b arduino:avr:uno "../Light Strip Controller/main"
 
 # Upload
 source .env
-/home/pi/arduino-cli upload -p $LED_STRIP_ARDUINO_SERIAL_PORT -b arduino:avr:uno "../Light Strip Controller/main" -t -v
+"$HOME"/arduino-cli upload -p "$LED_STRIP_ARDUINO_SERIAL_PORT" -b arduino:avr:uno "../Light Strip Controller/main" -t -v
 
 
 echo "Done!"
